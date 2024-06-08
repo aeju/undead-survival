9791ba9bcf77032d1e0487d197961b4a63d6e785
aeju <aejuchoe@naver.com>
2024-06-08 19:57:38 +0900

    삭제: Player 설명 주석
    기존 Input Manager 사용 코드 삭제

--- Assets/Undead Survivor/Codes/Player.cs
+++ Assets/Undead Survivor/Codes/Player.cs
@@ -15,24 +15,15 @@ public class Player : MonoBehaviour
         rigid = GetComponent<Rigidbody2D>(); // 초기화
     }
 
-    void FixedUpdate() // 물리 연산 프레임마다 호출
+    void FixedUpdate() // 물리 연산 프레임마다 호출 (위치 이동, 다른 프레임 환경에서도 이동 거리 같도록)
     {
-        // 위치 이동 (다른 프레임 환경에서도 이동 거리 같도록)
-        // Vector2 nextVec = inputVec.normalized * speed * Time.fixedDeltaTime; // fixedDeltaTime : 물리 프레임 하나 소비 시간 (FixedUpdate)
-        Vector2 nextVec = inputVec  * speed * Time.fixedDeltaTime; // 이미 InputSystem에서 normalized 해줘서, 빼도 o
+        // fixedDeltaTime : 물리 프레임 하나 소비 시간 (FixedUpdate)
+        Vector2 nextVec = inputVec * speed * Time.fixedDeltaTime; // 이미 InputSystem에서 normalize 해줘서, 해줄 필요 x
         rigid.MovePosition(rigid.position + nextVec); // 위치 이동 -> 현재 위치도 더해줘야 (nextVec = 방향)
     }
     
     void OnMove(InputValue value)
     {
-        inputVec = value.Get<Vector2>(); // 값이 다르기 때문에, = value (x)
+        inputVec = value.Get<Vector2>(); // Vector2 != InputValue (값 형식 다름)
     }
-    
-    /*
-    void Update()
-    {
-        inputVec.x = Input.GetAxisRaw("Horizontal"); // 더 명확한 컨트롤
-        inputVec.y = Input.GetAxisRaw("Vertical");
-    }
-    */
 }
