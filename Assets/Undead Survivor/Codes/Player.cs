60804af814630068e43ec369bdc0f5a303876382
aeju <aejuchoe@naver.com>
2024-06-08 19:53:39 +0900

    2-1. 새로운 인풋시스템 적용 (Input System)
    01 패키지 설치 (Input System) : 다양한 디바이스 인풋 쉽게 관리
    02 인풋 액션 설정
    - Create Action
    <Action Maps> : Player (UI)
    <Actions> : Move (Look, Fire)
    <Action Properties>
    - Action Type(인풋 형태) : Value (Button, Pass Through)
    - Control Type(값 형식) : Vector 2 (Vector3, Stick, Axis ...)
    - Interactions(호출 타이밍) : (Hold, Press, Slow Tap, Tap 기존 : 값이 변할 때만 가능)
    - Processors(후보정) : Normalize Vector 2 (Invert Vector2 - y축 반전, ...)
    03 스크립트 적용
    - 네임스페이스 추가: UnityEngine.InputSystem
    - 기존 Update 삭제
    - 사용할 액션 -> OnMove(), 매개변수 : InputValue
    - inputVec(Vector2) = value(InputValue).Get<Vector2>();
    - 기존 nextVec : normalized x

--- Assets/Undead Survivor/Codes/Player.cs
+++ Assets/Undead Survivor/Codes/Player.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
@@ -13,17 +14,25 @@ public class Player : MonoBehaviour
     {
         rigid = GetComponent<Rigidbody2D>(); // 초기화
     }
+
+    void FixedUpdate() // 물리 연산 프레임마다 호출
+    {
+        // 위치 이동 (다른 프레임 환경에서도 이동 거리 같도록)
+        // Vector2 nextVec = inputVec.normalized * speed * Time.fixedDeltaTime; // fixedDeltaTime : 물리 프레임 하나 소비 시간 (FixedUpdate)
+        Vector2 nextVec = inputVec  * speed * Time.fixedDeltaTime; // 이미 InputSystem에서 normalized 해줘서, 빼도 o
+        rigid.MovePosition(rigid.position + nextVec); // 위치 이동 -> 현재 위치도 더해줘야 (nextVec = 방향)
+    }
     
+    void OnMove(InputValue value)
+    {
+        inputVec = value.Get<Vector2>(); // 값이 다르기 때문에, = value (x)
+    }
+    
+    /*
     void Update()
     {
         inputVec.x = Input.GetAxisRaw("Horizontal"); // 더 명확한 컨트롤
         inputVec.y = Input.GetAxisRaw("Vertical");
     }
-
-    void FixedUpdate() // 물리 연산 프레임마다 호출
-    {
-        // 위치 이동 (다른 프레임 환경에서도 이동 거리 같도록)
-        Vector2 nextVec = inputVec.normalized * speed * Time.fixedDeltaTime; // fixedDeltaTime : 물리 프레임 하나 소비 시간 (FixedUpdate)
-        rigid.MovePosition(rigid.position + nextVec); // 위치 이동 -> 현재 위치도 더해줘야 (nextVec = 방향)
-    }
+    */
 }
