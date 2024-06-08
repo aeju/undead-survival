612c015dd7dd6637204a8406dedeedbe20cc93f1
aeju <aejuchoe@naver.com>
2024-06-09 01:39:24 +0900

    3. 2D 셀 애니메이션 제작하기
    01 방향 바라보기
    spriter.flipX = inputVec.x < 0
    02 셀 애니메이션 - 드래그 드랍으로 생성
    - Stand / Run / Dead
    03 애니메이터 설정
    - 조건 파라미터 : Speed - float / Dead - trigger
    - Duration : 부드럽게 전환 -> 2D : 0 or 0.01 (필요 x)
    - 즉시 상태 변경 : Has Exit Time 체크 해제
    - 반복 필요 x(Dead) : Loop Time 체크 해제
    04 코드 - 애니메이터 파라미터
    - SetFloat("파라미터 이름", float 값)
    - vector2.magnitude : 벡터의 순수 크기 값
    05 애니메이터 재활용
    - Animator Override Controller : 애니메이터 구조 그대로, 애니메이션만 덮어씌움

--- Assets/Undead Survivor/Codes/Player.cs
+++ Assets/Undead Survivor/Codes/Player.cs
@@ -9,21 +9,35 @@ public class Player : MonoBehaviour
     public float speed = 3;
 
     private Rigidbody2D rigid;
+    private SpriteRenderer spriter;
+    private Animator anim;
     
     void Awake()
     {
         rigid = GetComponent<Rigidbody2D>(); // 초기화
+        spriter = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
     }
 
     void FixedUpdate() // 물리 연산 프레임마다 호출 (위치 이동, 다른 프레임 환경에서도 이동 거리 같도록)
     {
-        // fixedDeltaTime : 물리 프레임 하나 소비 시간 (FixedUpdate)
-        Vector2 nextVec = inputVec * speed * Time.fixedDeltaTime; // 이미 InputSystem에서 normalize 해줘서, 해줄 필요 x
+        // 이미 InputSystem에서 normalize 해줘서, 해줄 필요 x
+        Vector2 nextVec = inputVec * speed * Time.fixedDeltaTime; // fixedDeltaTime : 물리 프레임 하나 소비 시간 
         rigid.MovePosition(rigid.position + nextVec); // 위치 이동 -> 현재 위치도 더해줘야 (nextVec = 방향)
     }
     
     void OnMove(InputValue value)
     {
         inputVec = value.Get<Vector2>(); // Vector2 != InputValue (값 형식 다름)
+    }
+
+    void LateUpdate() // 다음 프레임으로 넘어가기 직전에 실행
+    {
+        anim.SetFloat("Speed", inputVec.magnitude); // magnitude : 벡터의 순수한 크기 값
+        
+        if (inputVec.x != 0)
+        {
+            spriter.flipX = inputVec.x < 0; // 비교 연산자 결과 바로 넣기 (좌측키 = 마이너스 = 왼쪽)
+        }
     }
 }
