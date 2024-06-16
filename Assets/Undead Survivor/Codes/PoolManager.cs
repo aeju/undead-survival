d337017b3bdd229d0ba4595b11e1b90a6a0147fc
aeju <aejuchoe@naver.com>
2024-06-16 20:43:59 +0900

    6-3. [오브젝트 풀링으로 소환하기]풀링 함수 작성
    - foreach : 배열, 리스트 데이터 순차적으로 접근하는 반복문
    - 선택한 풀의 비활성화 된 게임오브젝트 접근
     1. 비활성화 상태 오브젝트 o -> select 변수에 할당
     1-1. 비활성화 오브젝트 : 활성화 (SetActive)
     2. 비활성화 상태 오브젝트 x -> 생성 로직 실행
     2-1. 새롭게 생성하고, select 변수에 할당
     2-2. 생성된 오브젝트, 오브젝트 풀 리스트에 추가

--- Assets/Undead Survivor/Codes/PoolManager.cs
+++ Assets/Undead Survivor/Codes/PoolManager.cs
@@ -22,4 +22,31 @@ public class PoolManager : MonoBehaviour
         
         Debug.Log(pools.Length);
     }
+
+    // 게임 오브젝트를 반환하는 함수 선언
+    public GameObject Get(int index) // 매개변수 : 가져올 오브젝트 종류 결정 
+    {
+        GameObject select = null;
+        
+        // ... 선택한 풀의 놀고 (비활성화 된) 있는 게임오브젝트 접근
+        foreach (GameObject item in pools[index]) // 배열, 리스트 데이터를 순차적으로 접근
+        {
+            if (!item.activeSelf) // 오브젝트가 비활성화(대기 상태)인지 확인
+            {
+                // ... 발견하면 select 변수에 할당
+                select = item;
+                select.SetActive(true); // 비활성화 오브젝트 찾으면, SetActive 함수로 활성화
+                break;
+            }
+        }
+            
+        // ...  못 찾았으면? -> 생성 로직
+        if (!select)
+        {
+            // ... 새롭게 생성하고 select 변수에 할당   
+            select = Instantiate(prefabs[index], transform); // Instantiate : 원본 오브젝트를 복제하여 장면에 생성
+            pools[index].Add(select); // 생성된 오브젝트, 해당 오브젝트 풀 리스트에 추가
+        }
+        return select;
+    }
 }
