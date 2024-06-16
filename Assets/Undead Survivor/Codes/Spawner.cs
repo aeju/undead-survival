568039d4c461c8c96201f1461a7a998caef912c7
aeju <aejuchoe@naver.com>
2024-06-16 22:12:58 +0900

    6-5. [오브젝트 풀링으로 소환하기]주변에 생성하기
    - spawner : 화면 밖에 배치
    ** spawnPoint : GetComponentsInChildern 함수로 초기화(Transform 가져옴)
    - GetComponentsInChilder : 컴포넌트를 가져오는 대상에 자신도 포함
    -> 이걸 사용하고, Random.Range 사용 -> (1, ~.Length)로 해야함

--- Assets/Undead Survivor/Codes/Spawner.cs
+++ Assets/Undead Survivor/Codes/Spawner.cs
@@ -4,11 +4,32 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
+    public Transform[] spawnPoint;
+
+    private float timer;
+
+    void Awake()
+    {
+        spawnPoint = GetComponentsInChildren<Transform>(); // GetComponentsInChildern : 자기 자신(Spawner 오브젝트)도 포함 -> 가장 아래 Random.Range에서 0대신 1!
+    }
+    
     void Update()
     {
-        if (Input.GetButtonDown("Jump"))
+        timer += Time.deltaTime; // 타이머 변수 : deltaTime 계속 더하기 
+
+        if (timer > 0.2f) // 타이머가 일정 시간 값에 도달하면 소환
         {
-            GameManager.instance.pool.Get(1); // 게임 매니저의 인스턴스에 접근, 풀링의 함수 호출
+            timer = 0;
+            // GameManager.instance.pool.Get(1);
+            Spawn(); // 소환 함수 새로 작성
         }
+    }
+
+    void Spawn()
+    {
+        //GameManager.instance.pool.Get(Random.Range(0, 2)); 
+        
+        GameObject enemy = GameManager.instance.pool.Get(Random.Range(0, 2)); // 변수에 담는 이유 : 한 번 더 이용할 것이기 때문 (아래줄 -> 만들어둔 소환 위치 중 하나로 배치되도록)
+        enemy.transform.position = spawnPoint[Random.Range(1, spawnPoint.Length)].position; // 자식 오브젝트에서만 선택되도록, 랜덤 시작 -> 1부터 
     }
 }
