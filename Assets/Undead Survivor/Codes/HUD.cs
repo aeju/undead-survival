afa6a30a2e7e2af15b6756cc45328a3981dc2cee
aeju <aejuchoe@naver.com>
2024-06-20 18:38:50 +0900

    10-5. [HUD 제작하기]타이머 텍스트 (Text)
    - Format("{0:D0}", 문자열)
    3) D0, D1, D2 ... : 자리 수를 지정
    cf. F0, F1 ... : 소수점 자리 지정

--- Assets/Undead Survivor/Codes/HUD.cs
+++ Assets/Undead Survivor/Codes/HUD.cs
@@ -32,7 +32,11 @@ public class HUD : MonoBehaviour
                 myText.text = string.Format("{0:F0}", GameManager.instance.kill);
                 break;
             case InfoType.Time:
-                
+                float remainTime = GameManager.instance.maxGameTime - GameManager.instance.gameTime; // 남은 시간
+                int min = Mathf.FloorToInt(remainTime / 60); // 소수점 버리기
+                int sec = Mathf.FloorToInt(remainTime % 60);
+                //myText.text = string.Format("{0:F0}:{1:F0}", min, sec); // 0:7
+                myText.text = string.Format("{0:D2}:{1:D2}", min, sec); // 00:07 (항상 두 자리) D0, D1 ... : 자리수 지정
                 break;
             case InfoType.Health:
                 
