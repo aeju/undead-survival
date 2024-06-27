7918bc019d4a829036e82b9fb15429555bde6d81
aeju <aejuchoe@naver.com>
2024-06-28 03:52:56 +0900

    16-5. [로직 보완하기]투사체 삭제 추가
    - OnTriggerExit2D : 플레이어 Area 밖으로 벗어나면 총알 비활성화

--- Assets/Undead Survivor/Codes/Bullet.cs
+++ Assets/Undead Survivor/Codes/Bullet.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
@@ -15,7 +16,6 @@ public class Bullet : MonoBehaviour
         rigid = GetComponent<Rigidbody2D>();
     }
     
-    //public void Init(float damage, int per)
     public void Init(float damage, int per, Vector3 dir) // 속도 매개변수 추가
     {
         this.damage = damage; // 왼쪽 : public damage, 오른쪽 : 매개변수
@@ -33,12 +33,19 @@ public class Bullet : MonoBehaviour
             return;
 
         per--; // 관통 감소
-
         // if (per == -1) // 관통 값이 하나씩 줄어들면서 -1이 되면 -> 비활성화
         if (per < 0) // 조건 느슨하게 변경 (더 안정적) 
         {
             rigid.velocity = Vector2.zero; // 미리 물리 속도 초기화
             gameObject.SetActive(false); // 풀링으로 관리
         }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision) // 비활성화 로직
+    {
+        if (!collision.CompareTag("Area") || per == -100)
+            return;
+        
+        gameObject.SetActive(false); // 플레이어가 가진 Area 밖으로 벗어나게 되면
     }
 }
